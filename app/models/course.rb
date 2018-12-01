dc805879b27a4bdac947ea9799738b4ea1624a96
bisquickman231 <whuskey6@gmail.ocm>
2018-12-01 16:35:22 -0700

    End of project one. Revert here to continue from a working project one.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,3 +2,4 @@ class Course < ApplicationRecord
   has_many :sections
 end
 #adding a comment to signify that project one has been reworked and finished
+# #adding a commit for timeline bookmarking REVERT HERE TO RESTART FROM PROJECT ONE
