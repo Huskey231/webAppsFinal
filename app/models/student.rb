6441a628b15bb815767cbe2f9ca5c422f2f52441
bisquickman231 <whuskey6@gmail.ocm>
2018-12-02 15:32:39 -0700

    Both student and sections shows the corresponding objects that belong to them in the view now.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   has_and_belongs_to_many :sections
+  #validates :name, presence: true #here to try and validate that a name is present in student creation
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #student = Student.new
+  #assert_not student.save,
 end
