18571e4178b77897e988b42b5eb9b68829614bdf
bisquickman231 <whuskey6@gmail.ocm>
2018-12-02 21:18:51 -0700

    Correctly implemented search to student.

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,17 @@ Rails.application.routes.draw do
     end
   end
   resources :sections do
+=begin
     collection do
       get 'search'
     end
+=end
   end
   resources :coursesdo
+=begin
   collection do
     get 'search'
   end
+=end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
